ff68e20f28b3f833ac2720802428c55992aab10b
Kwon Valerii <wellko24@gmail.com>
2023-03-14 14:52:59 +0600

    changed canvas for drawing circles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,29 @@
 import {useEffect, useRef, useState} from 'react'
 import {Coordinates, DrawDot, IncomingMessage} from "./types";
-import {Button, Grid, MenuItem, Select, SelectChangeEvent, Typography} from "@mui/material";
+import { Grid, MenuItem, Select, SelectChangeEvent, Typography} from "@mui/material";
 import Canvas from "./components/Canvas";
 
 function App() {
 	const [draw, setDraw] = useState<DrawDot[]>([]);
 	const [color, setColor] = useState('black');
-	const [isLoggedIn, setLoggedIn] = useState(false);
 	const ws = useRef<WebSocket | null>(null);
 	const colors = ['Black', 'DarkRed', 'FireBrick', 'Crimson', 'HotPink', 'DeepPink', 'MediumVioletRed', 'Tomato', 'OrangeRed', 'Gold', 'Magenta', 'Lime',
-		'MediumSpringGreen', 'Cyan', 'Blue', 'Peru']
+		'MediumSpringGreen', 'Cyan', 'Blue', 'Peru'];
 
 	useEffect(() => {
 		ws.current = new WebSocket('ws://localhost:8000/draw');
 		ws.current.onclose = () => console.log("ws closed");
+		ws.current.onopen = () => {
+			if (ws.current){
+				ws.current.send(JSON.stringify({
+					type: 'GET_DOTS'
+				}))
+			}
+		}
 		ws.current.onmessage = event => {
 			const decodedMessage = JSON.parse(event.data) as IncomingMessage;
 			if (decodedMessage.type === 'NEW_DOT') {
-				setDraw((dots) => [...dots, decodedMessage.payload]);
+				setDraw((dots) => dots.concat(decodedMessage.payload));
 			}
 		};
 		return () => {
@@ -27,12 +33,6 @@ function App() {
 		}
 	}, []);
 
-	useEffect(() => {
-		if(ws.current){
-
-		}
-	},[])
-
 	const changeColor = (e: SelectChangeEvent) => {
 		setColor(e.target.value);
 	};
@@ -41,47 +41,36 @@ function App() {
 		if (!ws.current) return;
 		ws.current.send(JSON.stringify({
 			type: 'SEND_DOT',
-			payload: JSON.stringify({
+			payload: [{
 				x: coords.x,
 				y: coords.y,
 				color: color,
-			})
+			}]
 		}));
-	}
+	};
 
 	const setUserColor = (e: React.FormEvent) => {
 		e.preventDefault();
-		if (!ws.current) return;
-		ws.current.send(JSON.stringify({
-			type: 'SET_COLOR',
-			payload: color
-		}));
-		setLoggedIn(true);
-		console.log(isLoggedIn)
 	};
 
 
 	return (
 		<>
-			{!isLoggedIn ?
+			<Typography variant='h2' textAlign='center'>You can draw here</Typography>
 				<form onSubmit={setUserColor}>
 					<Grid container textAlign='center'>
 						<Grid item xs={12}>
 							<Typography variant='h2' textAlign='center'>Choice color</Typography>
 						</Grid>
-						<Grid item margin='auto' mt={20}>
+						<Grid item margin='auto' mt={2}>
 							<Select value={color} onChange={changeColor} sx={{minWidth: '200px'}}>
 								{colors.map(el => <MenuItem key={Math.random()}
 															value={el.toLowerCase()}> {el} </MenuItem>)}
 							</Select>
-							<Button variant="contained" type='submit'>Join</Button>
 						</Grid>
 					</Grid>
 				</form>
-				: (<>
-					<Typography variant='h2' textAlign='center'>You can draw here</Typography>
 					<Canvas color={color} painted={draw} onDraw={onDraw}/>
-				</>)}
 		</>
 	)
 }
--- frontend/src/components/Canvas.tsx
+++ frontend/src/components/Canvas.tsx
@@ -2,114 +2,97 @@ import React, {useCallback, useEffect, useRef, useState} from 'react';
 import {Coordinates, DrawDot} from "../types";
 
 interface props {
-	color: string;
-	painted: DrawDot[];
-	onDraw(coords: Coordinates, color: string): void;
+    color: string;
+    painted: DrawDot[];
+    onDraw(coords: Coordinates, color: string): void;
 }
 
 
 const Canvas: React.FC<props> = ({color, painted, onDraw}) => {
-	const canvasRef = useRef<HTMLCanvasElement>(null);
-	const [isDrawing, setIsDrawing] = useState(false);
-	const [mousePosition, setMousePosition] = useState<Coordinates | undefined>(undefined);
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const [mousePosition, setMousePosition] = useState<Coordinates | undefined>(undefined);
 
-	const startPaint = useCallback((event: MouseEvent) => {
-		const coordinates = getCoordinates(event);
-		if (coordinates) {
-			setMousePosition(coordinates);
-			setIsDrawing(true);
-		}
-	}, []);
+    const startPaint = useCallback((event: MouseEvent) => {
+        const coordinates = getCoordinates(event);
+        if (coordinates) {
+            setMousePosition(coordinates);
+        }
+    }, []);
 
-	useEffect(() => {
-		if (!canvasRef.current) {
-			return;
-		}
-		const canvas: HTMLCanvasElement = canvasRef.current;
-		const context = canvas.getContext('2d');
-		if (context && painted) {
-			painted.map(el => draw({x: el.x, y: el.y}, el.color, false))
-		}
-	}, [painted])
+    useEffect(() => {
+        if (!canvasRef.current) {
+            return;
+        }
+        const canvas: HTMLCanvasElement = canvasRef.current;
+        const context = canvas.getContext('2d');
+        if (context && painted) {
+            painted.map(el => draw({x: el.x, y: el.y}, el.color, false))
+        }
+    }, [painted])
 
-	useEffect(() => {
-		if (!canvasRef.current) {
-			return;
-		}
-		const canvas: HTMLCanvasElement = canvasRef.current;
-		canvas.addEventListener('mousedown', startPaint);
-		return () => {
-			canvas.removeEventListener('mousedown', startPaint);
-		};
-	}, [startPaint]);
+    const paint = useCallback(
+        (event: MouseEvent) => {
+            const newMousePosition = getCoordinates(event);
+            if (mousePosition && newMousePosition) {
+                setMousePosition(newMousePosition);
+                draw(mousePosition, color, true);
+            }
+            setMousePosition(undefined);
+        },
+        [mousePosition]
+    );
 
-	const paint = useCallback(
-		(event: MouseEvent) => {
-			if (isDrawing) {
-				const newMousePosition = getCoordinates(event);
-				if (mousePosition && newMousePosition) {
-					setMousePosition(newMousePosition);
-					draw(mousePosition, color, true);
-				}
-			}
-		},
-		[isDrawing, mousePosition]
-	);
+    useEffect(() => {
+        if (!canvasRef.current) {
+            return;
+        }
+        const canvas: HTMLCanvasElement = canvasRef.current;
+        canvas.addEventListener('mousedown', startPaint);
+        return () => {
+            canvas.removeEventListener('mousedown', startPaint);
+        };
+    }, [startPaint]);
 
-	useEffect(() => {
-		if (!canvasRef.current) {
-			return;
-		}
-		const canvas: HTMLCanvasElement = canvasRef.current;
-		canvas.addEventListener('mousemove', paint);
-		return () => {
-			canvas.removeEventListener('mousemove', paint);
-		};
-	}, [paint]);
 
-	const exitPaint = useCallback(() => {
-		setIsDrawing(false);
-		setMousePosition(undefined);
-	}, []);
+    useEffect(() => {
+        if (!canvasRef.current) {
+            return;
+        }
+        const canvas: HTMLCanvasElement = canvasRef.current;
+        canvas.addEventListener('mouseup', paint);
+        return () => {
+            canvas.removeEventListener('mouseup', paint);
+        };
+    }, [paint]);
 
-	useEffect(() => {
-		if (!canvasRef.current) {
-			return;
-		}
-		const canvas: HTMLCanvasElement = canvasRef.current;
-		canvas.addEventListener('mouseup', exitPaint);
-		return () => {
-			canvas.removeEventListener('mouseup', exitPaint);
-		};
-	}, [exitPaint]);
+    const getCoordinates = (event: MouseEvent): Coordinates | undefined => {
+        if (!canvasRef.current) {
+            return;
+        }
+        const canvas: HTMLCanvasElement = canvasRef.current;
+        return {x: event.pageX - canvas.offsetLeft, y: event.pageY - canvas.offsetTop};
+    };
 
-	const getCoordinates = (event: MouseEvent): Coordinates | undefined => {
-		if (!canvasRef.current) {
-			return;
-		}
-		const canvas: HTMLCanvasElement = canvasRef.current;
-		return {x: event.pageX - canvas.offsetLeft, y: event.pageY - canvas.offsetTop};
-	};
+    const draw = (MousePosition: Coordinates, color: string, sending: boolean) => {
+        if (!canvasRef.current) {
+            return;
+        }
+        const canvas: HTMLCanvasElement = canvasRef.current;
+        const context = canvas.getContext('2d');
+        if (context) {
+            context.beginPath();
+            context.arc(MousePosition.x, MousePosition.y, 10, 0, 2 * Math.PI);
+            context.fillStyle = color;
+            context.fill();
+            context.closePath();
+            if (sending) {
+                onDraw(MousePosition, color);
+            }
+        }
+    };
 
-	const draw = (MousePosition: Coordinates, color: string, sending: boolean) => {
-		if (!canvasRef.current) {
-			return;
-		}
-		const canvas: HTMLCanvasElement = canvasRef.current;
-		const context = canvas.getContext('2d');
-		if (context) {
-			context.beginPath();
-			context.arc(MousePosition.x, MousePosition.y, 10, 0, 2 * Math.PI);
-			context.fillStyle = color;
-			context.fill();
-			context.closePath();
-			if(sending){
-				onDraw(MousePosition, color);
-			}
-		}
-	};
-
-	return <canvas ref={canvasRef} width={window.innerWidth} height='600' style={{borderTop: '2px solid black', borderBottom: '2px solid black'}}/>;
+    return <canvas ref={canvasRef} width={window.innerWidth} height='600'
+                   style={{borderTop: '2px solid black', borderBottom: '2px solid black'}}/>;
 };
 
 
